3c22e39aedcdf2b8cd212296956738f07d9a8162
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-11-07 23:26:45 +0530

    Added header. It contains the 'Enter Text' field and the 'Add' Button.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <header>
+      <h2>TO DO LIST</h2>
+        <form>
+          <input type="text" placeholder="Enter Text"></input>
+          <button type="submit">Add</button>
+        </form>
+      </header>
     </div>
   );
 }
