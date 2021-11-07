a59a1ab3e9f75e2b843504a0131e7b91ec7c18e6
Anushka Chauhan <anushkachauhan1503@gmail.com>
2021-11-07 23:32:14 +0530

    Added functionality to Add Button. handleInput and addItem methods.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,57 @@ import React from 'react';
 import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-      <header>
-      <h2>TO DO LIST</h2>
-        <form>
-          <input type="text" placeholder="Enter Text"></input>
-          <button type="submit">Add</button>
-        </form>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text !== "") {
+      const newItems=[...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+  }
+  render () {
+    return (
+      <div className="container">
+        <header>
+        <h2>TO DO LIST</h2>
+          <form onSubmit={this.addItem} >
+              <input type="text" placeholder="Enter Text" 
+                value={this.state.currentItem.text} 
+                onChange={this.handleInput}
+              />
+            <button type="submit">Add</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
